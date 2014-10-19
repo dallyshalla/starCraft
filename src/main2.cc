30b9cb28b13e714e4d289560b5733c09f2934245
Dally Shalla <dallyshalla@gmail.com>
2014-10-19 22:51:41 +0000

    save

--- src/main2.cc
+++ src/main2.cc
@@ -9,6 +9,7 @@ class member {
   int inventoryQuantities[4000];
   std::string gainedInventory[4000];
   int gainedQuantites[4000];
+  int locator;
   public:
     void buy(std::string);
     void sell(std::string);
@@ -34,13 +35,22 @@ int main()
   if(answer == 1)
   {
     std::cout << "What is your username to be?" << std::endl;
-    std::cin >> mem1.username;
-    std::cout << mem1.username << std::endl;  
+    std::cin >> mem1.username;  
     mem1.loadCoins(mem1.username);
-    std::cout << mem1.starCoins << std::endl;
+    std::cout << mem1.username << " you'll start off with " << mem1.starCoins << " starCoins, use them wisely" << std::endl;
   }
-  std::cout << mem1.username << " you'll start off with " << mem1.startCoins << " startCoins, use them wisely"
-    << std::endl;
+  std::cout << "You can make a second user and simulate exchange" << std::endl;
+  std::cout << "1. Start a new user" << std::endl;
+  std::cout << "0. Exit" << std::endl;
+  std::cin >> answer;
+  if(answer == 1) 
+  {
+    std::cout << "What is user2's name to be?" << std::endl;
+    std::cin >> mem2.username;
+    mem2.loadCoins(mem2.username);
+    std::cout << mem2.username << " you'll start off with " << mem2.starCoins << " starCoins, use them wisely" << std::endl; 
+  }
+  
 
   return 0;
 
