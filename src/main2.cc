c3a0364f066a3f34ee309ed7f12b81cb73492468
Dally Shalla <dallyshalla@gmail.com>
2014-10-19 07:57:39 +0000

    more more

--- src/main2.cc
+++ src/main2.cc
@@ -2,6 +2,7 @@
 #include <string>
 
 class member {
+  public:
   std::string username;
   std::string inventory[4000];
   int inventoryQuantities[4000];
@@ -16,12 +17,23 @@ class member {
     void storeNewItem(std::string);
 
 };
-
+int answer;
 int main()
 {
   member mem1;
   member mem2;
+  std::cout << "welcome to star Craft v0.0.1" << std::endl;
+  std::cout << "1. Start a username" << std::endl;
+  std::cout << "0. exit" << std::endl;
+  std::cin >> answer;
+  if(answer == 1)
+  {
+    std::cout << "What is your username to be?" << std::endl;
+    std::cin >> mem1.username;
+    std::cout << mem1.username << std::endl;
   
+  
+  }
   return 0;
 
 }
